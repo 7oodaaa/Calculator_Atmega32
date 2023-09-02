52af77bc16c69bf8f124ebc6ceb1aa330503177d
Mahmoud Yasser <138626978+7oodaaa@users.noreply.github.com>
2023-09-02 04:06:21 +0300

    Add files via upload

---
+++ BIT_MATH.h
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * @File           : BIT_MATH.h
+ * @Created on     : Aug 23, 2023
+ * @Author         : Mahmoud Yasser
+ ******************************************************************************
+ */
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define SET_BIT(REG,BIT) (REG |= (1<<BIT))
+#define CLR_BIT(REG,BIT) (REG &= (~(1<<BIT)))
+#define Toggle_Bit(REG,BIT) (REG ^= (1<<BIT))
+#define GET_BIT(REG,bit) ((REG>>bit)&1)
+
+#endif
---
+++ LCD_config.h
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * @File           : LCD_private.h
+ * @Created on     : Aug 23, 2023
+ * @Author         : Mahmoud Yasser
+ ******************************************************************************
+ */
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define LCD_PORT	Port_A
+#define LCD_RS		Pin1
+#define LCD_E		Pin2
+
+#endif /* LCD_CONFIG_H_ */
